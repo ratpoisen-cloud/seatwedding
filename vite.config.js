71e5e597938ca347f009e168939788c2ecdcac4c
Rat Poisen <ratpoisen@Rats-MacBook-Air.local>
2026-06-01 22:52:17 +0300

    Empty project for scratch rebuild

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/seatwedding/', // Adjust this to match your repository name
-  build: {
-    outDir: 'dist',
-  },
-});
